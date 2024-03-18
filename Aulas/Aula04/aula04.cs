3d03411f7e98f1765e8b922a058f7b07dacc3928
Sergio <alunosergio36@gmail.com>
2024-03-17 22:31:13 -0300

    Aula04

---
+++ Aulas/Aula04/aula04.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Aula04{
+   class Inicio{
+       static string num = "1";
+		
+		
+       static void Main(string[] args){
+            int num1 = 0;
+			Console.WriteLine(num);
+       }
+   } 
+}
