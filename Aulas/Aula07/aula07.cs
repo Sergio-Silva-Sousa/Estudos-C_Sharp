e217320f573699cae0e93517febfcbbad94a57fb
Sergio <alunosergio36@gmail.com>
2024-03-20 07:30:02 -0300

    Add Aula07

---
+++ Aulas/Aula07/aula07.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Aula07{
+    
+	class Program{
+        
+		static void Main(string[] args){
+            
+			const string name = "Sergio";
+			const int life = 1;
+			
+			Console.WriteLine($"Meu nome é {name} e eu só tenho uma {life} vida.");
+        }
+    }
+}
