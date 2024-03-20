04a6c1567ff358c80e966b5ca7f36bb15ff52446
Sergio <alunosergio36@gmail.com>
2024-03-19 21:52:26 -0300

    Add Aula05

---
+++ Aulas/Aula05/aula05.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Aula05{
+    class Programa{
+        
+		static void Main(string[] args){
+            
+			/*int res = 10+5; Atribuicao
+			bool res = 10<5; Relacionais 
+			bool res = 10!=10; Comparação
+			int num = 10;
+			
+			num = num + 1;
+			num += 1;
+			num++;
+			*/
+			// | = OR / OU
+			// & = AND / E
+			
+			bool res =(10>1) | (11>7);
+		
+			Console.WriteLine(res);  
+        }
+    }
+}
